d3f71c05eb701afcd95e53831847d2631d2b140d
Keisuke Matsuda <matsuda@wisesips.com>
2023-01-14 18:07:49 +0900

    ichimatsu_png

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use image;
 fn main() {
-    println!("Hello, world!");
+  // 白色をrgbで定義
+  let white = image::Rgb::<u8>([255, 255, 255]);
+  // 赤
+  let red = image::Rgb::<u8>([255, 90, 90]);
+  // 1マスのサイズ
+  let w = 64;
+  // 市松模様を描くクロージャー
+  let draw = |x, y| {
+    let (xi, yi) = (x / w, y / w);
+    match (xi % 2, yi % 2) {
+      (0, 0) => white,
+      (1, 0) => red,
+      (0, 1) => red,
+      (1, 1) => white,
+      (_, _) => panic!("error"),
+    }
+  };
+  // クロージャーを指定してImageBufferを生成
+  let img = image::ImageBuffer::from_fn(512, 512, draw);
+  // ファイルへ保存
+  img.save("ichimatsu.png").unwrap();
 }
+
